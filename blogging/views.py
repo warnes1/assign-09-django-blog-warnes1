89891eb5962642069bca70824229ced85d9ce497
[Scott Warner] <[warnes1@uw.edu]>
2023-05-29 20:41:05 -0700

    Final submission, Assignment 08, Unit Test working now

--- blogging/views.py
+++ blogging/views.py
@@ -8,25 +8,27 @@ from django.views.generic.detail import DetailView
 
 
 # No longer using, temporary use only
-def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args: \n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join(["\t%s: %s" % b for b in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")
+# def stub_view(request, *args, **kwargs):
+#     body = "Stub View\n\n"
+#     if args:
+#         body += "Args: \n"
+#         body += "\n".join(["\t%s" % a for a in args])
+#     if kwargs:
+#         body += "Kwargs:\n"
+#         body += "\n".join(["\t%s: %s" % b for b in kwargs.items()])
+#     return HttpResponse(body, content_type="text/plain")
 
 
 class PostListView(ListView):
 #    model = Post
-    context_object_name = 'post_list'
+#     context_object_name = 'post_list'
     queryset = Post.objects.order_by('-published_date').exclude(published_date__isnull=True)
     template_name = 'blogging/list.html'
 
 
 class PostDetailView(DetailView):
-    model = Post
-    template_name = 'blogging' \
-                    '/detail.html'
+    try:
+        queryset = Post.objects.exclude(published_date__isnull=True)
+        template_name = 'blogging/detail.html'
+    except Post.DoesNotExist:
+        raise Http404
