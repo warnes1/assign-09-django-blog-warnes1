2922f7590bd2ad528ec0e656fb3b63ecf9d52cbb
[Scott Warner] <[warnes1@uw.edu]>
2023-06-03 17:48:09 -0700

    Updated tests for blogging app

--- blogging/tests.py
+++ blogging/tests.py
@@ -50,7 +50,7 @@ class FrontEndTestCase(TestCase):
         resp = self.client.get('/')
         # the content of the rendered response is always a bytestring
         resp_text = resp.content.decode(resp.charset)
-        self.assertTrue("Recent Posts" in resp_text)
+        self.assertTrue("My Cool Blog Posts" in resp_text)
         for count in range(1, 11):
             title = "Post %d Title" % count
             if count < 6:
